237a099607c1070f77a206302d40f9ca12b212c5
Maxim Yarmolik <yarmoliq@gmail.com>
2020-10-18 16:18:41 +0300

    Redirections
    add: game controller
    add: game/index action and view
    add: game/find view
    
    change: buttons on home/index redirect to the things mentioned above
    
    remove: privacy action from home controller

---
+++ Controllers/GameController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using itr5.Models;
+
+namespace itr5.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public GameController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
